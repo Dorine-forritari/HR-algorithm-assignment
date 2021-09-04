c4deaf1adaeaed0b04a11c27871f5ec0a673cf92
Dorine van Belzen <dorinevanbelzen@gmail.com>
2021-09-04 22:03:06 +0000

    made progress on harmonic_mult assignment

--- harmonic_mult.py
+++ harmonic_mult.py
@@ -3,8 +3,17 @@ cmd = input("(h)armonic, (m)ultiplication or (q)uit: ")
 
 while cmd != "q":
 
-    # if cmd == "h":
+    if cmd == "h":
+        # Run ´harmonic series´ program
+        
+        n = input("Series length: ")
+        # The harmonic series has increasing divisors. Create variable ´divisor´. Set to default value of 0.
+        divisor = 0
+        # Create variable 'sum' that can be used to add the numbers of the series. Set to default value of 0.
+        sum = 0
 
+        # Use for-loop to print each number of the series and add its value to the sum.
+        for num in range
 
     # cmd = input("(h)armonic, (m)ultiplication or (q)uit: ")
 
