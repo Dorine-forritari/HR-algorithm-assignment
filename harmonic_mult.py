dbf102668a9ef3533aaeaa5ec7f18ff0c703b7b3
Dorine van Belzen <dorinevanbelzen@gmail.com>
2021-09-04 22:20:16 +0000

    finished harmonic series part of the harmonic_mult assignment

--- harmonic_mult.py
+++ harmonic_mult.py
@@ -5,17 +5,20 @@ while cmd != "q":
 
     if cmd == "h":
         # Run ´harmonic series´ program
-        
-        n = input("Series length: ")
-        # The harmonic series has increasing divisors. Create variable ´divisor´. Set to default value of 0.
-        divisor = 0
+
+        n = int(input("Series length: "))
+
         # Create variable 'sum' that can be used to add the numbers of the series. Set to default value of 0.
         sum = 0
 
         # Use for-loop to print each number of the series and add its value to the sum.
-        for num in range
-
-    # cmd = input("(h)armonic, (m)ultiplication or (q)uit: ")
+        # Since the harmonic series consists of numbers with increasing divisors (1/1, 1/2, 1/3, etc)..
+        # ..make sure to start the range at 1. End it at n + 1, so n is included.
+        for num in range(1, n+1):
+            sum += 1 / num
+            print(str(round(1 / num, 4)))
+        print(str(round(sum, 4)))
+        cmd = input("(h)armonic, (m)ultiplication or (q)uit: ")
 
    
    
