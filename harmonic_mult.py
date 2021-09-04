1963e53b233d9c76b3a22e386f6449eb624be72e
Dorine van Belzen <dorinevanbelzen@gmail.com>
2021-09-04 21:45:05 +0000

    added new file for harmonic_mult assignment

---
+++ harmonic_mult.py
@@ -0,0 +1,16 @@
+# Give user command options
+cmd = input("(h)armonic, (m)ultiplication or (q)uit: ")
+
+while cmd != "q":
+
+    # if cmd == "h":
+
+
+    # cmd = input("(h)armonic, (m)ultiplication or (q)uit: ")
+
+   
+   
+    # if cmd == "m":
+
+
+    # cmd = input("(h)armonic, (m)ultiplication or (q)uit: ")
